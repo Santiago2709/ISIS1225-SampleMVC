cf2b21dfe96b723ac351bacc3c1e99aecee75b78
Santiago Arciniegas <s.arciniegas@uniandes.edu.co>
2021-06-13 22:04:54 -0500

    Agregué la tercera opción al menu

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Book Tags")
     print("0- Salir")
 
 
